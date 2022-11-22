71828da1347fb6517bb0e28e4a94829152d2a729
Andres Martinez <amartinezcarrillo1006@gmail.com>
2022-11-22 19:33:09 -0300

    Aca agrego el primer motor de plantillas

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app= express()
 const productsRouter = require('./routes/products')
 const server = app.listen(8080,()=> console.log("Server up"))
 
 
 
+// config
+app.engine('handlebars', handlebars.engine())
+app.set('views', './views')
+app.set('view engine', 'handlebars')
+
+
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('views'))
 app.use(express.static('public'))
+
+
+
 app.use("/",productsRouter)
 app.use("/products",productsRouter)
+
+
+
--- routes/products.js
+++ routes/products.js
@@ -5,34 +5,48 @@ const contenedor = new Contenedor()
 const file = './products.txt'
 const products = contenedor.read(file)
 const multer = require('multer')
-router.use(express.static('public'))
+
+
+
+
+
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/img')
-    },
+    // destination: function (req, file, cb) {
+    //     cb(null, 'public/img')
+    // },
     filename: function (req, file, cb) {
         cb(null, file.originalname)
     }
 })
 
-const upload= router.use(multer({
+const upload = router.use(multer({
     storage,
     dest: "public/img"
 }).single('image'))
 
-const middlewareProducts = (req,res,next)=>{
-   let products= req.body
-    if(!products.article ||!products.price){
-        return res.status(400).send({Error:"Se requieren mas datos"})
-    }else{
+const middlewareProducts = (req, res, next) => {
+    let products = req.body
+    if (!products.article || !products.price) {
+        return res.status(400).send({
+            Error: "Se requieren mas datos"
+        })
+    } else {
 
         next()
     }
 }
 
-router.get('/products', (req, res) => {
-    res.json(contenedor.getAll(file))
+router.get('/', (req, res) => {
+    res.render('home')
 })
+
+
+router.get('/products', (req, res) => {
+res.render('products',{
+    products:products
+})
+})
+
 
 
 router.get('/products/:id', (req, res) => {
@@ -49,51 +63,51 @@ router.get('/products/:id', (req, res) => {
         });
 })
 
-router.post('/products',middlewareProducts, (req, res) => {
-    let product = req.body;
-    let imagen= req.file;
-    product.tumbnail= imagen.filename
+router.post('/products', middlewareProducts, (req, res) => {
+    const product = req.body;
+    // const imagen = req.file;
+    // product.thumbnail = imagen.filename
     contenedor.save(product, file)
-    res.json({Message: "Producto creado",product,file
-    })
+    // product.image =  '/uploads/'+imagen.filename;
+    res.redirect("products")
 })
 
 
-router.put('/products/:id', (req, res) => {
-    const {id} = req.params;
-    let body = req.body;
-    const product = contenedor.getById(parseInt(id), file);
-    if (product) {
-        contenedor.updateProduct(id, body, file)
-        res.json({
-            message: 'Producto actualizado',
-            producto: body
-        })
-    } else {
-        res.json({
-            message: 'No existe tal producto id :' + id
-        })
-    }
+// router.put('/products/:id', (req, res) => {
+//     const {id} = req.params;
+//     let body = req.body;
+//     const product = contenedor.getById(parseInt(id), file);
+//     if (product) {
+//         contenedor.updateProduct(id, body, file)
+//         res.json({
+//             message: 'Producto actualizado',
+//             producto: body
+//         })
+//     } else {
+//         res.json({
+//             message: 'No existe tal producto id :' + id
+//         })
+//     }
 
-})
+// })
 
 
-router.delete('/products/:id', (req, res) => {
-    const {
-        id
-    } = req.params;
-    const product = contenedor.deleteById(parseInt(id), file);
-    if (product) {
-        res.json({
-            message: 'Producto eliminado',
-            id: id
-        })
-    } else {
-        res.json({
-            message: 'Producto no encontrado. Id: ' + id
-        })
-    };
-});
+// router.delete('/products/:id', (req, res) => {
+//     const {
+//         id
+//     } = req.params;
+//     const product = contenedor.deleteById(parseInt(id), file);
+//     if (product) {
+//         res.json({
+//             message: 'Producto eliminado',
+//             id: id
+//         })
+//     } else {
+//         res.json({
+//             message: 'Producto no encontrado. Id: ' + id
+//         })
+//     };
+// });
 
 
 
